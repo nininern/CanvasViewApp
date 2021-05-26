242417c4e380c70019c081926de48e931f9aedbe
nininern <61041505+nininern@users.noreply.github.com>
2021-05-26 21:22:07 +0700

    CanvasViewApp

---
+++ app/src/main/java/com/example/canvasviewapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.canvasviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MyCanvasView myCanvasView;
+        myCanvasView=new MyCanvasView(this);
+        myCanvasView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
